ca0a13ad2934289720796c07bd0f8bb53d2fa65a
Yuhan-W <cnwangyuhan@gmail.com>
2022-09-27 15:17:34 -0500

    update batchnorm

--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -1,4 +1,5 @@
 from builtins import range
+from random import sample
 import numpy as np
 
 
@@ -193,8 +194,11 @@ def batchnorm_forward(x, gamma, beta, bn_param):
         # might prove to be helpful.                                          #
         #######################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-        pass
+        sample_mean = np.mean(x, axis=0)
+        sample_var = np.std(x, axis=0)
+        out = (x-sample_mean) / sample_var * gamma + beta
+        running_mean = momentum * running_mean + (1 - momentum) * sample_mean
+        running_var = momentum * running_var + (1 - momentum) * sample_var
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         #######################################################################
@@ -209,7 +213,7 @@ def batchnorm_forward(x, gamma, beta, bn_param):
         #######################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        out = (x-running_mean) / running_var * gamma + beta
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         #######################################################################
